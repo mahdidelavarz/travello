650a544222b12898c2dcb8fa757cf5671a41cbc2
mahdi delavar <mdelavar903@gmail.com>
2022-03-02 12:47:34 +0330

    is olmost done

---
+++ script/sctipt.js
@@ -0,0 +1,6 @@
+function getSearch(){
+const searchBox = document.querySelector(".searchForm");
+ searchBox.classList.toggle('active');
+const searchMenu =document.querySelector(".header-serch-box");
+ searchMenu.classList.toggle('unActive');
+}
