ad8e2edee6fb371fd77422ba58da450a95b05c52
mahdi delavar <mdelavar903@gmail.com>
2022-03-27 20:55:19 +0430

    back to top added

--- script/sctipt.js
+++ script/sctipt.js
@@ -4,3 +4,13 @@ const searchBox = document.querySelector(".searchForm");
 const searchMenu =document.querySelector(".header-serch-box");
  searchMenu.classList.toggle('unActive');
 }
+
+const goTop = document.querySelector(".goTop");
+
+window.addEventListener("scroll",() => {
+if(window.pageYOffset > 200){
+ goTop.style.opacity = 1;
+} else{
+ goTop.style.opacity = 0;
+}
+});
